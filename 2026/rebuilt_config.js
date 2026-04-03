69f3c1f75f140cd8809b97df9d7728823b23b7af
team5458scouting <team5458scouting@gmail.com>
2026-04-02 17:46:42 -0700

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026casac",
+      "defaultValue": "2026cacac",
       "required": "true"
     },
     { "name": "Match Level",
